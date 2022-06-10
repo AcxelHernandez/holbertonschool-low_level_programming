d8efc21482614db6d75ecd34ea3e1292f7e5e29c
AcxelHernandez <4263@holbertonschool.com>
2022-06-10 21:11:12 +0000

    Add task 4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print message followed by a new line
+ * Return: Return 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
