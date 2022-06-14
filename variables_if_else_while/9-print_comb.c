503666c90c7757e0f9f2975f308b3533b8090898
AcxelHernandez <4263@holbertonschool.com>
2022-06-14 21:30:08 +0000

    Add task 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single numbers of base 10
+ * starting from 0, numbers separated by ,
+ * followed by space, printed in ascending order
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+		if(num < 57){
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
