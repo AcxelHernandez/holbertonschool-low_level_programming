739560042143c48c459bbc17562c72d87941bb4c
AcxelHernandez <4263@holbertonschool.com>
2022-06-14 16:32:00 +0000

    Add task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * print all the letters except q and e
+ * Return: 0 
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		if( ch!='q' || ch!='e' ){
+			putchar(ch);
+		}
+
+	putchar('\n');
+	return (0);
+}
+
