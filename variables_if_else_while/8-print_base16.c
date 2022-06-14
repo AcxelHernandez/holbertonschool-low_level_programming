9e340e5e0a639aadd70b1b3ffd69c41a490a253c
AcxelHernandez <4263@holbertonschool.com>
2022-06-14 20:53:44 +0000

    Add task 8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase,
- * new line
+ * main - prints the all numbers of base 16 in lowercase,
+ * only use putchar tree times 0-9 a-f
  * Return: 0
  */
 
 int main(void)
 {
-	char ch, i;
+	char i, ch;
+
 	for (i = '0'; i <= '9'; i++)
 		putchar(i);
 	for (ch = 'a'; ch <= 'f'; ch++)
 		putchar(ch);
+
 	putchar('\n');
 	return (0);
 }
