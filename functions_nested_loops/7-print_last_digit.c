9e944ddbd9c25b879ab741e0d7d15e9637df24b1
AcxelHernandez <4263@holbertonschool.com>
2022-06-15 03:49:09 +0000

    Add task 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * print_last_digit - function that print last digit
+ * @d: input int
+ * Description: last digit of number
+ * Return: value of the last digit
+ */
+
+int _abs(int d)
+{
+	if (d >= 0)
+		return (d);
+	else
+		return (-d);
+}
