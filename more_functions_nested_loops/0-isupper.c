3bf53d12ebae219f14f5474106a191eecd8376c7
AcxelHernandez <4263@holbertonschool.com>
2022-06-15 04:15:15 +0000

    Add task 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 
 /**
  * _isupper - a function that checks for uppercase character
- * @c: input
+ * @c: input checks
  * Return: 1 if c is uppercase, 0 otherwise
  * similar function: isupper
  */
