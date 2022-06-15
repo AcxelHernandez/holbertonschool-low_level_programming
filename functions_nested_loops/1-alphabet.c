6f5edf7818e323c5c3fe7a2ec0df8cf2e65254bc
AcxelHernandez <4263@holbertonschool.com>
2022-06-15 02:27:11 +0000

    Add task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * main - function that prints the alphabet,
+ * in lowercase, followed by a new line
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+
+	_putchar('\n');
+}
