f32cca2dd52cbdd15abd440b61cccbc2c36dff46
AcxelHernandez <4263@holbertonschool.com>
2022-06-15 03:41:34 +0000

    Add task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * _abs - function that computes
+ * @h: character checked
+ * Description: in lowercase or uppercase
+ * Return: absolute value of an integer
+ */
+
+int _abs(int h)
+{
+	if (h >= 0)
+		return (h);
+	else
+		return (-h);
+}
