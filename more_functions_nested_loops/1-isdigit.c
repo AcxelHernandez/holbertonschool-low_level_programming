2c50a6154045cd997dbcc87be0065a0d37408d5f
AcxelHernandez <4263@holbertonschool.com>
2022-06-22 15:03:14 +0000

    Fix Code

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -3,13 +3,14 @@
 /**
  * _isdigit - function that checks for a digit 0 through 9
  * @c: input int  checks
- * Return: 1 if c is digit, 0 otherwise
+ * Description: check input for digit 0 - 9
  * similar function: isdigit
+ * Return: 1 if c is digit, 0 otherwise
  */
 
 int _isdigit(int c)
 {
-	if ((c > 0) && (c < 9))
+	if ((c >= '0') && (c <= '9'))
 		return (1);
 	else
 		return (0);
