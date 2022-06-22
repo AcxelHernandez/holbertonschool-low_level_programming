d2bc8228f390d56d97fa9aeced2f5a3b135a639c
AcxelHernandez <4263@holbertonschool.com>
2022-06-22 13:16:12 +0000

    Add task 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,11 +2,11 @@
 
 /**
  * _isupper - a function that checks for uppercase character
- * @c: input checks
+ * @c: input int  checks
  * Return: 1 if c is uppercase, 0 otherwise
  * similar function: isupper
  */
-int _isupper(char c)
+int _isupper(int c)
 {
 	if ((c >= 'A') && (c <= 'Z'))
 		return (1);
